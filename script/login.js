1ac0bfb64c3a58faf2d9fe1b9f89283e0616a7f9
gazisaiful07 <imsaiful2047@gmail.com>
2026-03-09 00:11:38 +0600

    login functionality updated

--- script/login.js
+++ script/login.js
@@ -5,10 +5,10 @@ document.getElementById("login-btn").addEventListener("click", function () {
     // get username and password
     const inputUserName = document.getElementById("input-username");
     const userName = inputUserName.value;
-    console.log(userName)
+    // console.log(userName)
     const inputPassword = document.getElementById("input-password");
     const userPassword = inputPassword.value;
-    console.log(userPassword)
+    // console.log(userPassword)
 
     if (userName == "admin" && userPassword == "admin123") {
         alert("Login Success")
