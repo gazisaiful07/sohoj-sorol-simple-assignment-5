caa1fc779f0857e7c3ed9d72fdc2df72ad509a65
gazisaiful07 <imsaiful2047@gmail.com>
2026-03-09 00:06:44 +0600

    login funcotionality added with js

--- script/login.js
+++ script/login.js
@@ -1,16 +1,20 @@
 console.log("login hoise")
 
 
-document.getElementById("login-btn").addEventListener("click", function(){
+document.getElementById("login-btn").addEventListener("click", function () {
     // get username and password
     const inputUserName = document.getElementById("input-username");
     const userName = inputUserName.value;
     console.log(userName)
-    const inputPassword = document.getElementById ("input-password");
+    const inputPassword = document.getElementById("input-password");
     const userPassword = inputPassword.value;
     console.log(userPassword)
 
-    if (userName=="admin" && userPassword=="admin123"){
+    if (userName == "admin" && userPassword == "admin123") {
         alert("Login Success")
     }
-})
+    else {
+        alert("Failed to Sign In");
+        return
+    }
+});
