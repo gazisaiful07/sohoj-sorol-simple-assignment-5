cee990223fb75202b6a8ce86df53b37251d58a88
gazisaiful07 <imsaiful2047@gmail.com>
2026-03-09 08:48:56 +0600

    add issue cards, status filtering and summary section

--- script/main.js
+++ script/main.js
@@ -1,12 +1,42 @@
 const issuesContainer = document.getElementById("issues-container")
+const openCount = document.getElementById("open-count");
+const closedCount = document.getElementById("closed-count");
+const totalIssues = document.getElementById("total-issues");
+
+console.log(openCount)
+
+
+
 let allIssuesData = [];
 async function allIssues() {
     const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
     const data = await res.json()
     allIssuesData = data.data;
+    
+
+    updateStatus();
     showIssues(allIssuesData);
 };
+
+function setActiveButton(status){
+
+    document.getElementById("btn-all").classList.remove("btn-primary");
+    document.getElementById("btn-open").classList.remove("btn-primary");
+    document.getElementById("btn-closed").classList.remove("btn-primary");
+
+    document.getElementById("btn-all").classList.add("btn-outline");
+    document.getElementById("btn-open").classList.add("btn-outline");
+    document.getElementById("btn-closed").classList.add("btn-outline");
+
+    document.getElementById(`btn-${status}`).classList.remove("btn-outline");
+    document.getElementById(`btn-${status}`).classList.add("btn-primary");
+}
+
+function updateStatus() {
+    totalIssues.innerText = `${allIssuesData.length} Issues`;
+}
 function filterIssues(status) {
+    setActiveButton(status)
     if (status === "all") {
         showIssues(allIssuesData);
         return;
@@ -19,20 +49,63 @@ function filterIssues(status) {
 function showIssues(issues) {
     issuesContainer.innerHTML = ``;
     issues.forEach(issue => {
-        const div = document.createElement("div")
-        div.className = "border p-4 rounded-lg mb-2 bg-red-700";
-        div.innerHTML = `
-            <h3 class="font-bold">${issue.title}</h3>
-        <p>Status: ${issue.status}</p>
-            `;
-        issuesContainer.appendChild(div)
+        const card = document.createElement("div")
+        const isOpen = issue.status.toLowerCase() === "open";
+        card.className = card.className = `bg-white rounded-xl shadow-sm border border-blue-50 bg-white overflow-hidden  gap-20
+                        ${isOpen ? "border-t-4 border-t-green-500" : "border-t-4 border-t-purple-500 border"}`;
+        card.innerHTML = `
+                <div class="p-5">
+                <div class="flex justify-between items-center mb-4 ">
+                    <div class="w-8 h-8 rounded-full flex items-center justify-center text-sm gap-20
+                        ${isOpen ? "bg-green-100 text-green-600" : "bg-purple-100 text-purple-600"}">
+                        ${isOpen ? "<image src=./assets/Open-Status.png>" : '<i class="fa-solid fa-circle-check"></i>'}
+                    </div>
+
+                    <span class="px-4 py-1 rounded-full text-sm font-medium
+                        ${issue.priority?.toLowerCase() === "high"
+                ? "bg-red-100 text-red-500"
+                : issue.priority?.toLowerCase() === "medium"
+                    ? "bg-yellow-100 text-yellow-600"
+                    : "bg-gray-100 text-gray-500"
+            }">
+                        ${issue.priority || "LOW"}
+                    </span>
+                </div>
+
+                <h3 class="text-2xl font-semibold mb-3 leading-snug">
+                    ${issue.title || "No title"}
+                </h3>
+
+                <p class="text-gray-500 mb-4">
+                    ${issue.description || "No description available"}
+                </p>
+
+                <div class="flex flex-wrap gap-2 mb-5">
+                    ${(issue.labels || [])
+                .map(
+                    label => `
+                            <span class="px-3 py-1 rounded-full text-sm border
+                                ${label.toLowerCase().includes("bug")
+                            ? "bg-red-50 text-red-500 border-red-200"
+                            : "bg-yellow-50 text-yellow-600 border-yellow-300"
+                        }">
+                                ${label}
+                            </span>
+                        `
+                )
+                .join("")}
+                </div>
+            </div>
+
+            <div class="border-t-3  border-gray-100 px-5 py-4 text-gray-500">
+                <p>#${issue.id || 1} by ${issue.author || "john_doe"}</p>
+                <p class="mt-1">${issue.created_at || "1/15/2024"}</p>
+            </div>
+                `;
+        issuesContainer.appendChild(card)
     });
 
 
 };
-
-
-
-
 allIssues()
-filterIssues()
+
